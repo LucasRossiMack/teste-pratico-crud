6c654020220ee0c8d24cff581f1951bcbac030d0
lucasrossioliveira <lucasrossioliveira@gmail.com>
2024-09-27 18:51:09 -0300

    fix: adiciona preço nos produtos

--- teste-pratico-crud-api/Models/Produto.cs
+++ teste-pratico-crud-api/Models/Produto.cs
@@ -4,6 +4,7 @@ public class Produto
 {
     public long Id { get; set; }
     public string Nome { get; set; }
+    public double Preco { get; set; }
     public string Descricao { get; set; }
     public int Quantidade { get; set; }
     public string Categoria { get; set; }
