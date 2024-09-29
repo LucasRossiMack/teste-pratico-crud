54be38c81ca30d2094951625c862b724b8c83241
lucasrossioliveira <lucasrossioliveira@gmail.com>
2024-09-29 14:44:28 -0300

    feat: get produtos

--- teste-pratico-crud-front/teste-pratico-crud/src/app/app.component.ts
+++ teste-pratico-crud-front/teste-pratico-crud/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProdutosService } from './sercices/produtos.service';
+import { Produto } from './models/produto.model';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,16 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'teste-pratico-crud';
+
+  produtos: Produto[] = [];
+
+  constructor(private produtosService : ProdutosService){
+    this. obterProdutosCadastrados();
+  }
+
+  obterProdutosCadastrados(){
+    this.produtosService.obterProdutos()
+      .subscribe(produtos => this.produtos = produtos)
+  }
+
 }
--- teste-pratico-crud-front/teste-pratico-crud/src/app/sercices/produtos.service.ts
+++ teste-pratico-crud-front/teste-pratico-crud/src/app/sercices/produtos.service.ts
@@ -1,17 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
+import { Produto } from '../models/produto.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProdutosService {
 
+  private url = environment.api;
 
   constructor(private httpClient: HttpClient) {
    }
 
    obterProdutos(){
-
+    return this.httpClient.get<Produto[]>(this.url+'Produtos')
    }
 }
