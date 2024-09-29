6015bb22d152a33ec5085e3794c8e4849dd4d796
lucasrossioliveira <lucasrossioliveira@gmail.com>
2024-09-29 13:59:33 -0300

    feat: configurando httpclient

--- teste-pratico-crud-front/teste-pratico-crud/src/app/sercices/produtos.service.ts
+++ teste-pratico-crud-front/teste-pratico-crud/src/app/sercices/produtos.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProdutosService {
 
-  constructor() { }
+
+  constructor(private httpClient: HttpClient) {
+   }
+
+   obterProdutos(){
+
+   }
 }
